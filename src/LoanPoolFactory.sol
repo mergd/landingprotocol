853eee9579d1a9e1a119a499621c47f231c89e20
William Xuan <williamxuan@MacBook-Air.local>
2023-05-29 12:23:51 -0700

    add factory

---
+++ src/LoanPoolFactory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./ILenderInterface.sol";
+
+contract LoanPoolFactory {
+    constructor() {}
+    // implement default factory here?
+}
